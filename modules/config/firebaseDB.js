4af7d69bcbaad2e20655e3347854308a33050262
Claudio Rodriguez <cjrodr@yahoo.com>
2017-08-15 11:34:40 +0100

    fix: fix for new firebase

--- modules/config/firebaseDB.js
+++ modules/config/firebaseDB.js
@@ -3,27 +3,26 @@
 const firebase = require('firebase');
 
 function getAuthFirebase() {
-  if (!process.env.FIREBASE_DBNAME ||
+  if (!process.env.FIREBASE_API_KEY ||
       !process.env.FIREBASE_PROJECT_ID ||
-      !process.env.FIREBASE_EMAIL ||
-      !process.env.FIREBASE_PRIVATE_KEY) {
+      !process.env.FIREBASE_DATABASE_NAME ||
+      !process.env.FIREBASE_SENDER_ID) {
     return Promise.reject(new Error('No firebase persistance configured'));
   }
 
   let db;
-  const firebaseServiceAccount = {
+  const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: `${process.env.FIREBASE_PROJECT_ID}.firebaseapp.com`,
+    databaseURL: `https://${process.env.FIREBASE_DATABASE_NAME}.firebaseio.com`,
     projectId: process.env.FIREBASE_PROJECT_ID,
-    privateKey: JSON.parse(`"${process.env.FIREBASE_PRIVATE_KEY}"`), // unescape escape chars
-    clientEmail: process.env.FIREBASE_EMAIL,
+    storageBucket: process.env.FIREBASE_BUCKET || '',
+    messagingSenderId: process.env.FIREBASE_SENDER_ID
   };
 
   try {
-    firebase.initializeApp({
-      databaseURL: `https://${process.env.FIREBASE_DBNAME}.firebaseio.com/`,
-      serviceAccount: firebaseServiceAccount
-    });
+    firebase.initializeApp(firebaseConfig);
     db = firebase.database();
-
   } catch(err) {
     return Promise.reject(err);
   }
